54be0824d518fb02e490f23e8e2a617f5233ed56
oli ullah jewel <oli.jewel@northsouth.edu>
2021-04-25 17:44:09 +0600

    Implemented fade-in and fade-out classes,

--- js/script.js
+++ js/script.js
@@ -1,16 +1,26 @@
 const btnHamburger = document.querySelector("#btnHamburger");
+const noScroll = document.querySelector("body");
 const header = document.querySelector(".header");
 const overlay = document.querySelector(".overlay");
+const fadeElems = document.querySelectorAll(".has-fade");
 
 btnHamburger.addEventListener("click", function(){
 
     if(header.classList.contains("open")){
+        noScroll.classList.remove("noscroll");
         header.classList.remove("open");
-        overlay.classList.remove("fade-in");
-        overlay.classList.add("fade-out");
+        fadeElems.forEach(function(elements){
+            elements.classList.remove("fade-in");
+            elements.classList.add("fade-out");
+        })
+        
     }else{
+        noScroll.classList.add("noscroll");
         header.classList.add("open");
-        overlay.classList.remove("fade-out");
-        overlay.classList.add("fade-in");
+        fadeElems.forEach(function(elements){
+            elements.classList.remove("fade-out");
+           elements.classList.add("fade-in");      
+        })
+        
     }
 })
